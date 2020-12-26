822bd039b923abbc4a1580acdda993a93c8829d9
christian-p-anderson <christian.p.anderson1@gmail.com>
2020-12-26 18:51:36 -0500

    created welcome component

--- frontend/todo/src/app/app.component.ts
+++ frontend/todo/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: '<h1>{{title}}</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo';
+  message = 'My name is Christian. It\'s nice to meet you!';
+  question = 'How can I help you, today?';
 }
