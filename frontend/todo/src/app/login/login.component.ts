50aea47bded4b1cf291e8496b56bafcf19728415
christian-p-anderson <christian.p.anderson1@gmail.com>
2020-12-26 22:33:15 -0500

    added notes to login component about the 3 types of data binding

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -16,7 +16,14 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:typedef
   handleLogin() {
     console.log(this.username);
+    console.log(this.password);
   }
 }
+// Three kinds of data binding
+// 1. Interpolation where we tie a component property to a view element
+// 2. Event binding - where we tie a user event to a event which is defined to something in the .component.ts file
+// 3. Two-Way data binding implemented using [(ngModel)] - whatever value is presented to the component property is tied to the
+// view element and what ever data is entered in the view element is automatically tied back to the component property
