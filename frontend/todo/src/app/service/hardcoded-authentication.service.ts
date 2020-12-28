0a00e3e8082f11902e21159829490edbd05b8f0d
christian-p-anderson <christian.p.anderson1@gmail.com>
2020-12-28 05:36:39 -0500

    recorded in session storage whether user is logged in

--- frontend/todo/src/app/service/hardcoded-authentication.service.ts
+++ frontend/todo/src/app/service/hardcoded-authentication.service.ts
@@ -10,8 +10,15 @@ export class HardcodedAuthenticationService {
   // tslint:disable-next-line:typedef
   authenticate(username: string, password: string) {
     if (username === 'in28minutes' && password === 'dummy') {
+      sessionStorage.setItem('authenticatedUser', username); // stores information in browser's session storage
       return true;
     }
     return false;
+  }
+
+  // tslint:disable-next-line:typedef
+  isUserLoggedIn() {
+    const user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
   }
 }
