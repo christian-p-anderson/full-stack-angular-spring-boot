4588490f0856ba681916f8d266771cb08a50d2c1
christian-p-anderson <christian.p.anderson1@gmail.com>
2020-12-28 15:55:37 -0500

    created a GET request using a path variable

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/Controllers/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/Controllers/HelloWorldController.java
@@ -3,13 +3,12 @@ package com.rest.webservices.restfulwebservices.Controllers;
 
 import com.rest.webservices.restfulwebservices.Models.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // Controller
 @RestController
 public class HelloWorldController {
-
-
 
     @GetMapping("hello-world")
     public String helloWorld() {
@@ -20,5 +19,11 @@ public class HelloWorldController {
     @GetMapping("hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    //hello-world/path-variable/in28Mintues
+    @GetMapping("hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
