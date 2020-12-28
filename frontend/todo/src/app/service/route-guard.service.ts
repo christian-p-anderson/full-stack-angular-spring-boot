4870cb56b5b525c3fcff5c15a56f0af67e926f49
christian-p-anderson <christian.p.anderson1@gmail.com>
2020-12-28 06:27:02 -0500

    redirect to login if attempt to access links assoc with logged in user

--- frontend/todo/src/app/service/route-guard.service.ts
+++ frontend/todo/src/app/service/route-guard.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {HardcodedAuthenticationService} from './hardcoded-authentication.service';
 
 @Injectable({
@@ -7,7 +7,8 @@ import {HardcodedAuthenticationService} from './hardcoded-authentication.service
 })
 export class RouteGuardService implements CanActivate {
 
-  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService) {
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService,
+              private router: Router) {
   }
 
   // tslint:disable-next-line:typedef
@@ -15,6 +16,7 @@ export class RouteGuardService implements CanActivate {
     if (this.hardcodedAuthenticationService.isUserLoggedIn()) {
       return true;
     }
+    this.router.navigate(['login']);
     return false;
   }
 }
