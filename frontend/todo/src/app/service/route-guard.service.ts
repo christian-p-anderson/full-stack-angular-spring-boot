06e03fa12649a58cca79af8a7987d296e23f2f9f
christian-p-anderson <christian.p.anderson1@gmail.com>
2021-01-01 13:42:53 -0500

    tied route-guard service to basic authentication service

--- frontend/todo/src/app/service/route-guard.service.ts
+++ frontend/todo/src/app/service/route-guard.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {HardcodedAuthenticationService} from './hardcoded-authentication.service';
+import {BasicAuthenticationService} from './basic-authentication.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,19 @@ import {HardcodedAuthenticationService} from './hardcoded-authentication.service
 export class RouteGuardService implements CanActivate {
 
   constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService,
+              private basicAuthenticationService: BasicAuthenticationService,
               private router: Router) {
   }
-
-  // tslint:disable-next-line:typedef
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (this.hardcodedAuthenticationService.isUserLoggedIn()) {
+  canActivate = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+    if (this.basicAuthenticationService.isUserLoggedIn()) {
       return true;
     }
     this.router.navigate(['login']);
     return false;
+  //   if (this.hardcodedAuthenticationService.isUserLoggedIn()) {
+  //     return true;
+  //   }
+  //   this.router.navigate(['login']);
+  //   return false;
   }
 }
