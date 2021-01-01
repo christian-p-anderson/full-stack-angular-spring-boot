569a5d0896f040c4b5899fc3d0e32151484dbcec
christian-p-anderson <christian.p.anderson1@gmail.com>
2020-12-31 22:07:57 -0500

    configured Spring Security to disable CSRF and enable OPTION Requests

--- frontend/todo/src/app/service/data/welcome-data.service.ts
+++ frontend/todo/src/app/service/data/welcome-data.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHandler, HttpHeaders} from '@angular/common/http';
 
 export class HelloWorldBean {
-  constructor(public message: string) {}
+  constructor(public message: string) {
+  }
 }
+
 // create class to show what you are expecting back
 
 @Injectable({
@@ -14,14 +16,35 @@ export class WelcomeDataService {
   // Angular has HttpClient - this will allow us to communicate to java backend
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+  }
+
   // tslint:disable-next-line:typedef
   executeHelloWorldBeanService() {
     return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world-bean');
   }
 
+  // Step 77
   // tslint:disable-next-line:typedef
   executeHelloWorldServiceWithPathVariable(name: any) {
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`);
+    const basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+    const headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+    });
+    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`,
+      {
+        headers
+      });
   }
+
+  // tslint:disable-next-line:typedef
+  createBasicAuthenticationHttpHeader() {
+    const username = 'in28minutes';
+    const password = 'dummy';
+    const basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    return basicAuthHeaderString;
+  }
+
+//  Access to XMLHttpRequest at
+//  http://localhost:8080/hello-world/path-variable/in28minutes
 }
